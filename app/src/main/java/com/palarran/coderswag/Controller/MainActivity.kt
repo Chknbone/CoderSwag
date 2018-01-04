73fce29327edecec00bdcad0430153ab4456f2e3
Chknbone <cb@palarran.com>
2018-01-04 19:42:10 +1100

    Lesson 3

--- app/src/main/java/com/palarran/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/palarran/coderswag/Controller/MainActivity.kt
@@ -2,22 +2,21 @@ package com.palarran.coderswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import com.palarran.coderswag.Model.Category
+import com.palarran.coderswag.Adapters.CategoryAdapter
 import com.palarran.coderswag.R
 import com.palarran.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-	lateinit var adapter: ArrayAdapter<Category>
+	lateinit var adapter: CategoryAdapter //this is the custom adapter created in CategoryAdapter.kt
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		//adapter with all data (context, where the data is going, where data is coming from)
-		adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+		//adapter with all data (context, where data is coming from)
+		adapter = CategoryAdapter(this, DataService.categories)
 
 		//tell listView who/where it needs to listen to
 		categoryListView.adapter = adapter
