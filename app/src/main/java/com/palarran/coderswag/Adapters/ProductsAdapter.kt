84065ba9d182e77a97bae223529e976ef99cc745
Chknbone <cb@palarran.com>
2018-01-06 17:57:37 +1100

    Lesson 8

---
+++ app/src/main/java/com/palarran/coderswag/Adapters/ProductsAdapter.kt
@@ -0,0 +1,43 @@
+package com.palarran.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.palarran.coderswag.Model.Product
+import com.palarran.coderswag.R
+
+class ProductsAdapter(val context: Context, val products: List<Product>) : Adapter<ProductsAdapter
+.ProductHolder>(){
+
+	override fun onBindViewHolder(holder: ProductHolder?, position: Int) {
+		holder?.bindProduct(products[position], context)
+	}
+
+	override fun getItemCount(): Int {
+		return products.count()
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProductHolder {
+		val view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+		return ProductHolder(view)
+	}
+
+	inner class ProductHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+		val productImage = itemView?.findViewById<ImageView>(R.id.productImage)
+		val prodcutName = itemView?.findViewById<TextView>(R.id.productName)
+		val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
+
+		fun bindProduct(product: Product, context: Context) {
+			val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+			productImage?.setImageResource(resourceId)
+			prodcutName?.text = product.title
+			productPrice?.text = product.price
+		}
+	}
+}
--- app/src/main/java/com/palarran/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/palarran/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,39 @@
 package com.palarran.coderswag.Controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import com.palarran.coderswag.Adapters.ProductsAdapter
 import com.palarran.coderswag.R
+import com.palarran.coderswag.Services.DataService
 import com.palarran.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+
+	lateinit var adapter : ProductsAdapter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_products)
 
 		val categoryType =  intent.getStringExtra(EXTRA_CATEGORY)
+
+		//returns the category type/option that was clicked on in the MainActivity
+		adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+		var spanCount = 2
+		val orientation = resources.configuration.orientation
+		if (orientation == Configuration.ORIENTATION_LANDSCAPE) spanCount = 3
+
+		val screenSize = resources.configuration.screenWidthDp
+		if (screenSize > 720) spanCount = 3
+
+		//layoutManager is required when using Recyclerview
+		val layoutManager = GridLayoutManager(this, 2)
+		productListView.layoutManager = layoutManager
+		productListView.adapter = adapter
 	}
 }
